dc59ee20fdc44e88c56d6c3c790632f444d6788b
João Vitor Pereira <joao.vitor@zarbsolution.com.br>
2020-10-25 15:45:06 -0300

    new: Adicao de lib react-native-design-utility para trabalhar com um tema em todo o app, atuando como um provider
    new: Adicao da lib do react-navigatione  configurando a navegação em stack

---
+++ App/Containers/Home/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Box, Text} from 'react-native-design-utility';
+
+const Home: React.FC = () => {
+	return (
+		<Box f={1} center>
+			<Text size={'lg'} color="green">
+				HomeScreen
+			</Text>
+		</Box>
+	);
+};
+
+export default Home;
